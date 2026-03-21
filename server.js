71c65ff3a5dce968f9f7f551f79a66b107ba0f0f
pligamstag <162299019+Pligamstag@users.noreply.github.com>
2026-03-21 21:54:59 +0100

    Fix static file serving path in server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors({
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(path.join(__dirname, './')));
 
 app.use('/api/posts',    postRoutes);
 app.use('/api/profiles', profileRoutes);
