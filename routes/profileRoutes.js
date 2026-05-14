1b360be11dcd86a2129779564e5bcfafbdea2158
pligamstag <162299019+Pligamstag@users.noreply.github.com>
2026-05-14 23:11:09 +0200

    Add admin route to profileRoutes

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -56,5 +56,10 @@ router.put('/me', protect, async (req, res) => {
     res.status(500).json({ message: 'Erreur serveur.' });
   }
 });
+router.get'/admin', async (req, res) => {
+  try {
+    res.render('admin');
+  } catch { res.status(500).json({ message: 'Erreur.' }); }
+});
 
 module.exports = router;
